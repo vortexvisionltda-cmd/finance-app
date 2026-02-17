de0e18fd15cc199fa97da079eaabdf9e03f2950b
vortexvisionltda-cmd <vortexvisionltda@gmail.com>
2026-02-17 12:53:19 -0300

    Update Supabase URL in supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,9 @@
-const SUPABASE_URL = "https://qbklzlircfrkgzsehwoo.supabase.com";
+const SUPABASE_URL = "https://qbklzlircfrkgzsehwoo.supabase.co";
 const SUPABASE_KEY = "sb_publishable_OZLJa-Xx1Fra88SYAI90_w_sKZm-vIh";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
+
 
