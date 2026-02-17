7f4a01581324dc3d97f36f5074782f0ba7211f12
vortexvisionltda-cmd <vortexvisionltda@gmail.com>
2026-02-17 13:15:26 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "https://qbklzircfkrgzsehwoo.supabase.co";
-const SUPABASE_KEY = "sb_publishable_OZLJa-Xx1Fra88SYAI90_w_sKZm-vIh";
-
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
-
-
